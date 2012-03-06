c58d9991e99c1120ac0e95a0ddfc775078666afc
thebestsophist <bernardy@enguin.com>
2012-03-06 17:37:55 -0600

    Moving to folder
    Moving to its own folder for easy of git
    
    Signed-off-by: thebestsophist <bernardy@enguin.com>

---
+++ screen_detection/extension.driver.php
@@ -0,0 +1,50 @@
+<?php
+
+Class Extension_ScreeDetection extends Extension {
+
+	public function about() {
+    	return array(
+    		'author'   	=> array(
+    	  		'email'   => 'hello@enguin.com',
+    			'name'	=> 'Enguin Design',
+    			'website' => 'http://enguindesign.com'
+    		),
+    		'description'  => 'This extention adds user screen information to the symphony param pool for server-side optimizations for responsive web design.',
+    		'name' 		=> 'Screen Detection',
+    		'release-date' => '2012-03-06',
+    		'version'  	=> '2012.03.01'
+    	);
+    }
+
+	public function install() {
+	  	Symphony::Configuration()->set('screen_detection_enabled', 'true', 'screen_detection');
+	}
+
+	public function uninstall() {
+  		Symphony::Configuration()->remove('screen_detection');
+	}
+
+	public function getSubscribedDelegates(){
+  		return array(
+		
+			array(
+  				'page' 	=> '/frontend/',
+  				'delegate' => 'FrontendParamsResolve',
+  				'callback' => 'addParam'
+			)
+		);
+	}
+
+
+	public function addParam($context) {
+
+		$screen_height = !empty($_COOKIE['screen-height']) ? $_COOKIE['screen-height'] : 0;
+		$screen_width =  !empty($_COOKIE['screen-width']) ? $_COOKIE['screen-width'] : 0;
+		$screen_orientation = !empty($_COOKIE['screen-orientation']) ? $_COOKIE['screen-orientation'] : 0;
+		
+
+	}
+
+}
+
+?>
