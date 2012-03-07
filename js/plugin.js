fdd6b71dae5c4ade44f5041b2c92fa5d0286dfb4
thebestsophist <bernardy@enguin.com>
2012-03-07 04:38:01 -0600

    Adding cookie for screen size
    Adding cookies for screen width and height. Renaming window width and height cookies to be what they mean.
    
    Signed-off-by: thebestsophist <bernardy@enguin.com>

--- js/plugin.js
+++ js/plugin.js
@@ -1,6 +1,8 @@
 // This requires jQuery to function properly
 // Sets window properties as cookies for server-side optimizations
 $(window).resize(function () {
-	document.cookie = "screen-height=" + $(window).height() + "; path=/";
-	document.cookie = "screen-width=" + $(window).width() + "; path=/";
+	document.cookie = "window-width=" + $(window).width() + "; path=/";
+	document.cookie = "window-height=" + $(window).height() + "; path=/";
+	document.cookie = "screen-width=" + screen.width + "; path=/";
+	document.cookie = "screen-height" + screen.height + "; path=/";
 });
