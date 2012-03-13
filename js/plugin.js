b1369e062d2c3be239d024fedd0f2279883da8b3
thebestsophist <bernardy@enguin.com>
2012-03-13 13:31:25 -0500

    Woo! It works!
    Updates create cookies on ready, then updates on window resize and screen resize.
    
    Signed-off-by: thebestsophist <bernardy@enguin.com>

--- js/plugin.js
+++ js/plugin.js
@@ -1,20 +1,33 @@
 // This requires jQuery to function properly
 // Sets window properties as cookies for server-side optimizations
-$(window).resize(function () {
 
-var pixel-density = window.devicePixelRatio || 1,
+function getDevicePixelRatio() {
+	if(window.devicePixelRatio === undefined) return 1; // No pixel ratio available. Assume 1:1.
+	return window.devicePixelRatio;
+}
 
+function getWindowOrientation() {
+	if(window.orientation === undefined) return 0;
+	return window.orientation;
+}
 
+function getWindowProperties() {
 	document.cookie = "window-width=" + $(window).width() + "; path=/";
 	document.cookie = "window-height=" + $(window).height() + "; path=/";
+	document.cookie = 'screen-orientation=' + getWindowOrientation() + '; path=/';
+}
+
+function getScreenProperties() {
 	document.cookie = "screen-width=" + screen.width + "; path=/";
 	document.cookie = "screen-height=" + screen.height + "; path=/";
-	document.cookie = "pixel-density=" + pixel-density + "; path=/";
+	document.cookie = "pixel-density=" + getDevicePixelRatio() + "; path=/";
 
+}
 
+// Sets window properties as cookies for server-side optimizations
+$(document).ready(function(){
+	getWindowProperties();
+	getScreenProperties();
 });
-
-window.addEventListener(orientationEvent, function() {
-	window-orientation = window.orientation || 0,
-	document.cookie = 'screen-orientation=' + window-orientation + '; path=/';
-});
+$(window).resize(getWindowProperties);
+$(screen).resize(getScreenProperties);
